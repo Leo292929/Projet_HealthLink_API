973dc0fdf2b0b9de399c2b5aab043622eb8cbb8a
Leo292929 <leo.fougeroud@ecoles-epsi.net>
2024-10-09 12:22:08 +0200

    gestion des ports pour render

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from app import create_app
-
+import os
 # Créer l'instance de l'application Flask
 app = create_app()
 
 if __name__ == '__main__':
     # Démarrer le serveur Flask en mode debug (ou pas)
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port,debug=True)
